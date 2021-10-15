8d9a5f49ae5dd44873b2bd0db3580e887a537d0a
Daniel Coker <danielcoker28@outlook.com>
2021-10-15 15:38:12 +0100

    Use `gettext_lazy` instead of deprecated `ugettext_lazy`

--- users/models.py
+++ users/models.py
@@ -6,7 +6,7 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import (
     AbstractBaseUser, BaseUserManager, PermissionsMixin)
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 from base.models import TimestampedModel
